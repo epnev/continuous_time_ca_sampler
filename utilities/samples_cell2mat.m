1257d99d924081c2b6432e028ace431c2f2c803e
epnev <eap2111@columbia.edu>
2014-05-07 19:05:10 -0400

    minor changes when creating raster plot
    Added option for specifying resolution when constructing the raster

--- utilities/samples_cell2mat.m
+++
@@ -1,9 +0,0 @@
-function spikeRaster = samples_cell2mat(sampleCell,T)
-    nsamples = length(sampleCell);
-    spikeRaster = zeros(nsamples,T);
-    for i = 1:nsamples
-        tmp = histc([sampleCell{i} inf],[0:(T-1) (T+1)]);
-        %size(spikeRaster(i,:))
-        %size(tmp(1:(end-1))')
-        spikeRaster(i,:) = tmp(1:(end-1))';
-    end
