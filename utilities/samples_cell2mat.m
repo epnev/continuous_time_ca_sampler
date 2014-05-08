790c3529372d42d04d2d530368355722dd79881c
epnev <eap2111@columbia.edu>
2014-05-08 13:44:44 -0400

    minor change

--- utilities/samples_cell2mat.m
+++ utilities/samples_cell2mat.m
@@ -7,7 +7,5 @@ function spikeRaster = samples_cell2mat(sampleCell,T,Dt)
     spikeRaster = zeros(nsamples,length(bins));
     for i = 1:nsamples
         tmp = histc([sampleCell{i}(:); inf],[bins, (T+1)]);
-        %size(spikeRaster(i,:))
-        %size(tmp(1:(end-1))')
         spikeRaster(i,:) = tmp(1:(end-1))';
     end
