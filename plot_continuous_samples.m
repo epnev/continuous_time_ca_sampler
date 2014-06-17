c489c83b1173b0e8fa0729deb85b4c26978322eb
epnev <eap2111@columbia.edu>
2014-06-17 18:39:27 -0400

    Include AR(2) support
    Modified the code to support finite rise time through a second order
    autoregressive process

--- plot_continuous_samples.m
+++ plot_continuous_samples.m
@@ -2,15 +2,36 @@ function plot_continuous_samples(SAMPLES,P,Y)
 
 T = length(Y);
 N = length(SAMPLES.ns);
-show_gamma = 1;
+show_gamma = 0;
 P.f = 1;
+g = P.g;
+p = length(g);
 Dt = 1/P.f;                                     % length of time bin
 if ~isfield(SAMPLES,'g');
     show_gamma = 0;
-    SAMPLES.g = P.g*ones(N,1);
+    SAMPLES.g = ones(N,1)*g';
 end
-g = SAMPLES.g;
-tau = -Dt./log(g);
+
+if p == 1
+    tau_1 = 0;
+    tau_2 = -Dt/log(g);                         % continuous time constant
+    G1 = speye(T);
+    G2 = spdiags(ones(T,1)*[-g,1],[-1:0],T,T);
+    ge = P.g.^((0:T-1)');     
+elseif p == 2
+    gr = roots([1,-g']);
+    p1_continuous = log(min(gr))/Dt; 
+    p2_continuous = log(max(gr))/Dt;
+    tau_1 = -1/p1_continuous;                   %tau h - smaller (tau_d * tau_r)/(tau_d + tau_r)
+    tau_2 = -1/p2_continuous;                   %tau decay - larger
+    G1 = spdiags(ones(T,1)*[-min(gr),1],[-1:0],T,T);
+    G2 = spdiags(ones(T,1)*[-max(gr),1],[-1:0],T,T);
+    ge = [-G1\[1;zeros(T-1,1)],G2\[1;zeros(T-1,1)]];
+else
+    error('This order of the AR process is currently not supported');
+end
+
+
 
 if length(SAMPLES.Cb) == 2
     marg = 1;       % marginalized sampler
@@ -20,12 +41,16 @@ end
 
 C_rec = zeros(N,T);
 for rep = 1:N
-    trunc_spikes = ceil(SAMPLES.ss{rep}/Dt);
-    s_ = sparse(1,trunc_spikes,exp((SAMPLES.ss{rep} - Dt*trunc_spikes)/tau(rep)),1,T);
+    %trunc_spikes = ceil(SAMPLES.ss{rep}/Dt);
+    s_1 =   sparse(ceil(SAMPLES.ss{rep}/Dt),1,exp((SAMPLES.ss{rep} - Dt*ceil(SAMPLES.ss{rep}/Dt))/tau_1),T,1);  
+    s_2 =   sparse(ceil(SAMPLES.ss{rep}/Dt),1,exp((SAMPLES.ss{rep} - Dt*ceil(SAMPLES.ss{rep}/Dt))/tau_2),T,1);  
+    Gs = (-G1\s_1(:)+ G2\s_2(:));
     if marg
-        C_rec(rep,:) = SAMPLES.Cb(1) + SAMPLES.Am(rep)*filter(1,[1,-g(rep)],full(s_)+[SAMPLES.Cin(1),zeros(1,T-1)]);
+        %C_rec(rep,:) = SAMPLES.Cb(1) + SAMPLES.Am(rep)*filter(1,[1,-SAMPLES.g(rep,:)],full(s_)+[SAMPLES.Cin(:,1)',zeros(1,T-p)]);
+        C_rec(rep,:) = SAMPLES.Cb(1) + SAMPLES.Am(rep)*Gs + (ge*SAMPLES.Cin(:,1));
     else
-        C_rec(rep,:) = SAMPLES.Cb(rep) + SAMPLES.Am(rep)*filter(1,[1,-g(rep)],full(s_)+[SAMPLES.Cin(rep),zeros(1,T-1)]);
+        %C_rec(rep,:) = SAMPLES.Cb(rep) + SAMPLES.Am(rep)*filter(1,[1,-SAMPLES.g(rep,:)],full(s_)+[SAMPLES.Cin(rep,:),zeros(1,T-p)]);
+        C_rec(rep,:) = SAMPLES.Cb(rep) + SAMPLES.Am(rep)*Gs + (ge*SAMPLES.Cin(rep,:)');
     end
 end
 Nc = 60;
@@ -65,8 +90,14 @@ figure;
         subplot(4,4,15); plot(xx,normpdf(xx,SAMPLES.Cb(1),SAMPLES.Cb(2)));
             set(gca,'XLim',[xx(1),xx(end)])
             title('Marg. post. of baseline','fontweight','bold','fontsize',14)
-        xx = SAMPLES.Cin(1) + linspace(-4*SAMPLES.Cin(2),4*SAMPLES.Cin(2));
-        subplot(4,4,16); plot(xx,normpdf(xx,SAMPLES.Cin(1),SAMPLES.Cin(2)));
+        if p == 1
+            xx = SAMPLES.Cin(1) + linspace(-4*SAMPLES.Cin(2),4*SAMPLES.Cin(2));
+            subplot(4,4,16); plot(xx,normpdf(xx,SAMPLES.Cin(1),SAMPLES.Cin(2)));
+        else
+            xx = linspace(min(SAMPLES.Cin(1),SAMPLES.Cin(2))-4*min(SAMPLES.Cin(3),SAMPLES.Cin(4)),max(SAMPLES.Cin(1),SAMPLES.Cin(2)) + 4*max(SAMPLES.Cin(3),SAMPLES.Cin(4)));
+            subplot(4,4,16); plot(xx,normpdf(xx,SAMPLES.Cin(1),SAMPLES.Cin(3))); hold all;
+                             plot(xx,normpdf(xx,SAMPLES.Cin(2),SAMPLES.Cin(4)));
+        end
             set(gca,'XLim',[xx(1),xx(end)])
             title('Marg. post. of initial con','fontweight','bold','fontsize',14)
     else
@@ -74,7 +105,11 @@ figure;
         subplot(5,4,4+12); plot(-Nc:Nc,xcov(SAMPLES.Cb,Nc,'coeff')); set(gca,'XLim',[-Nc,Nc])
             title('Autocorrelation','fontweight','bold','fontsize',14)
         subplot(5,4,4+13); plot(1:N,SAMPLES.Cin); title('Initial Concentration','fontweight','bold','fontsize',14)
-        subplot(5,4,4+14); plot(-Nc:Nc,xcov(SAMPLES.Cin,Nc,'coeff')); set(gca,'XLim',[-Nc,Nc])
+        xcov_Cin = xcov(SAMPLES.Cin,Nc,'coeff');
+        if p == 2
+            xcov_Cin = xcov_Cin(:,[1,4]);
+        end
+        subplot(5,4,4+14); plot(-Nc:Nc,xcov_Cin); set(gca,'XLim',[-Nc,Nc])
             title('Autocorrelation','fontweight','bold','fontsize',14)
         subplot(5,4,4+15); plot(1:N,SAMPLES.sn2); title('Noise variance','fontweight','bold','fontsize',14)
         subplot(5,4,4+16); plot(-Nc:Nc,xcov(SAMPLES.sn2,Nc,'coeff')); set(gca,'XLim',[-Nc,Nc])
--- utilities/addSpike.m
+++ utilities/addSpike.m
@@ -1,15 +1,38 @@
 function [newSpikeTrain, newCalcium, newLL] = addSpike(oldSpikeTrain,oldCalcium,oldLL,filter,tau,obsCalcium,timeToAdd,Dt,A)
 
+    tau_h = tau(1);
+    tau_d = tau(2);
+    
+    ef_h = filter{1};
+    ef_d = filter{2};
+    
     newSpikeTrain = [oldSpikeTrain timeToAdd]; %possibly inefficient, change if problematic (only likely to be a problem for large numbers of spikes)
     
     %use infinite precision to scale the precomputed FIR approximation to the calcium transient
-    wk = A*exp((timeToAdd - Dt*ceil(timeToAdd/Dt))/tau);
+    wk_h = A*exp((timeToAdd - Dt*ceil(timeToAdd/Dt))/tau_h);
+    wk_d = A*exp((timeToAdd - Dt*ceil(timeToAdd/Dt))/tau_d);
     
+    %%%%%%%%%%%%%%%%%
+    %handle ef_h first
     newCalcium = oldCalcium;
-    tmp = 1 + (floor(timeToAdd):min((length(filter)+floor(timeToAdd)-1),length(newCalcium)-1));
-    newCalcium(tmp) = newCalcium(tmp) + wk*filter(1:length(tmp));
+    tmp = 1 + (floor(timeToAdd):min((length(ef_h)+floor(timeToAdd)-1),length(newCalcium)-1));
+    newCalcium(tmp) = newCalcium(tmp) + wk_h*ef_h(1:length(tmp));
     
     %if you really want to, ef*ef' could be precomputed and passed in
     relevantResidual = obsCalcium(tmp)-oldCalcium(tmp);
+    newLL = oldLL - ( wk_h^2*norm(ef_h(1:length(tmp)))^2 - 2*relevantResidual*(wk_h*ef_h(1:length(tmp))'));
+    oldCalcium = newCalcium;
+    oldLL = newLL;
+    %%%%%%%%%%%%%%%%%
     
-    newLL = oldLL - ( wk^2*norm(filter(1:length(tmp)))^2 - 2*relevantResidual*(wk*filter(1:length(tmp))'));
+    %%%%%%%%%%%%%%%%%
+    %handle ef_d next
+    tmp = 1 + (floor(timeToAdd):min((length(ef_d)+floor(timeToAdd)-1),length(newCalcium)-1));
+    newCalcium(tmp) = newCalcium(tmp) + wk_d*ef_d(1:length(tmp));
+    
+    %if you really want to, ef*ef' could be precomputed and passed in
+    relevantResidual = obsCalcium(tmp)-oldCalcium(tmp);
+    newLL = oldLL - ( wk_d^2*norm(ef_d(1:length(tmp)))^2 - 2*relevantResidual*(wk_d*ef_d(1:length(tmp))'));
+    %%%%%%%%%%%%%%%%%
+    
+    
